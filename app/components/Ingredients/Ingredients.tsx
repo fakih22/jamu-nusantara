4f5e482da0e20c6feb426db5312f64af724dcc4b
unknown <mr.riuuus@gmail.com>
2026-06-30 11:51:58 +0700

    upds

--- app/components/Ingredients/Ingredients.tsx
+++ app/components/Ingredients/Ingredients.tsx
@@ -10,7 +10,7 @@ const ingredients = [
   {
     latin: 'Zingiber Officinale',
     badge: 'Hangat',
-    badgeColor: 'amber',
+    badgeColor: 'amber wood',
     name: 'Jahe Merah',
     desc: 'Gingerol tinggi, melancarkan sirkulasi & mengurangi mual.',
     origin: 'Asal: Pacitan • Varietas: Dandang Merah putih.'
