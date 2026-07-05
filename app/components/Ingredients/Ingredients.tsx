3f9584a09cca9e2043d3502f8cfee5f544fcd772
unknown <mr.riuuus@gmail.com>
2026-07-05 19:59:50 +0700

    zas

--- app/components/Ingredients/Ingredients.tsx
+++ app/components/Ingredients/Ingredients.tsx
@@ -21,7 +21,7 @@ const ingredients = [
     badgeColor: 'emeranld',
     name: 'Sereh',
     desc: 'Citral alami, segar, bantu detoksifikasi & menenangkan',
-    origin: 'Asal: Jogja • Petuk Sonten.'
+    origin: 'Asal: Jogja • Petuk Sontens.'
   },
   {
     latin: 'Tamarindus Indira',
