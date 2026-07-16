5ee788ffc1084c473b5cc6e0306d25ece806f23d
unknown <mr.riuuus@gmail.com>
2026-07-16 17:36:24 +0700

    update 3142

--- app/components/Ingredients/Ingredients.tsx
+++ app/components/Ingredients/Ingredients.tsx
@@ -37,7 +37,7 @@ const ingredients = [
     badgeColor: 'ambers',
     name: 'Temulawak',
     desc: 'Tonik hati tradisional, bantu nafsu makan',
-    origin: 'Asal: Gunung Kidul.'
+    origin: 'Asal: Gunung Kiduls.'
   },
   {
     latin: 'Cinnamomum Verums',
